ccb36a0bc696c8b7e013c58d654937f215f8a2e7
Asser <assertamer2002@gmail.com>
2024-08-28 18:01:35 +0300

    architecture fix

--- lib/features/main_screen/presentation/widgets/screens_build.dart
+++ lib/features/main_screen/presentation/widgets/screens_build.dart
@@ -6,7 +6,7 @@ import 'package:my_store/features/main_screen/presentation/favourites_tab/favour
 import 'package:my_store/features/main_screen/presentation/favourites_tab/favourites_screen.dart';
 import 'package:my_store/features/main_screen/presentation/home_tab/home_cubit/home_cubit.dart';
 import 'package:my_store/features/main_screen/presentation/home_tab/home_screen.dart';
-import 'package:my_store/features/profile_tab/profile_screen.dart';
+import 'package:my_store/features/main_screen/presentation/profile_tab/profile_screen.dart';
 import 'package:my_store/features/main_screen/presentation/search_tab/cubit/search_cubit.dart';
 import 'package:my_store/features/main_screen/presentation/search_tab/search_screen.dart';
 
